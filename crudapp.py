53420d42b676c54de193e5f91cfd01665c337ef1
afmirandadiaz <amirandadiaz@godaddy.com>
2024-08-20 10:42:09 -0500

    Read method creation

--- crudapp.py
+++ crudapp.py
@@ -13,8 +13,21 @@ def create_connection(config):
         print(f"This error '{e}'  occured")
     return connection
 
+# Method to read salaries
+def read_salaries(connection):
+    cursor = connection.cursor()
+    query = "select * from salaries where salary > 100000 limit 10"
+    cursor.execute(query)
+    salaries = cursor.fetchall()
+    for salary in salaries:
+        print("Salary: "+str(salary))
+
 def main():
     connection = create_connection(config)
+
+    # Calling method read
+    print("Read salaries upper 100000")
+    read_salaries(connection)
 
 if __name__ == "__main__":
     main()
