5f203f588e091c830e985946b9754e0088407ac7
AndresMiranda <61157532+afmirandad@users.noreply.github.com>
2024-08-20 10:22:39 -0500

    Update crudapp.py

--- crudapp.py
+++ crudapp.py
@@ -4,9 +4,9 @@ from mysql.connector import Error
 
 # Database configuration
 config = {
-    'host':'18.191.204.59',
-    'user':'studentsucundi',
-    'password':'mami_prende_la_radi0',
+    'host':'',
+    'user':'',
+    'password':'',
     'database':'employees'
 }
 
