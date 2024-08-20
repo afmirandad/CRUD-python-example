513ec242497907f0b4c63829208827d25f0e5e06
afmirandadiaz <amirandadiaz@godaddy.com>
2024-08-20 13:05:37 -0500

    Final commit

--- crudapp.py
+++ crudapp.py
@@ -30,8 +30,32 @@ def create_salaries(connection, emp_no, salary, from_date, to_date):
     connection.commit()
     print(f"New salary for employee '{emp_no}' has been inserted.")
 
+# Method to update a registry
+def update_employee(connection, emp_no, salary, from_date, to_date):
+    cursor = connection.cursor()
+    query = """
+    UPDATE salaries 
+    SET salary = %s, from_date = %s, to_date = %s
+    WHERE emp_no = %s
+    """
+    cursor.execute(query, (salary, from_date, to_date,emp_no))
+    connection.commit()
+    print(f"Employee {emp_no} updated successfully.")
+
+# Method to delete an employee
+def delete_employee(connection, emp_no):
+    cursor = connection.cursor()
+    query = "DELETE FROM salaries WHERE emp_no = %s"
+    cursor.execute(query, (emp_no,))
+    connection.commit()
+    print(f"Employee {emp_no} deleted successfully.")
+
 def main():
     connection = create_connection(config)
+
+    # Calling method delete
+    print("Delete employee")
+    delete_employee(connection,999999)
 
     # Calling method create a new salary
     create_salaries(connection,999999,130000,'1985-10-20','1986-10-20')
@@ -39,6 +63,10 @@ def main():
     # Calling method read
     print("Read salaries upper 100000")
     read_salaries(connection)
+
+    # Calling method update
+    print("Update employee data")
+    update_employee(connection,999999,130000,'1985-10-20','1986-10-20')
 
 if __name__ == "__main__":
     main()
