2803c011775560fb9b25e526b77125fd68bc9229
afmirandadiaz <amirandadiaz@godaddy.com>
2024-08-30 10:35:33 -0500

    Final commit

--- crudmongo.py
+++ crudmongo.py
@@ -19,11 +19,20 @@ def readone():
     newread = tabla.find_one({'_id':ObjectId('66d1dfbcef692062b07389d8')})
     if newread:
         newread['_id'] = str(newread['_id'])
-        print(newread['nombre'])
+        print(newread)
     else:
         print("No hay datos")
 
+def updateone():
+    data = {'apellido':'diaz'}
+    updateone = tabla.update_many({'_id':ObjectId('66d1dfbcef692062b07389d8')},{'$set':data})
+    readone()
+
+def deleteone():
+    newread = tabla.delete_one({'_id': ObjectId('66d1dfbcef692062b07389d8')})
+    readone()
 
 if __name__ == '__main__':
     createone()
     readone()
+    updateone()
