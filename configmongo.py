3207d9f90228169ab30584960f79b5d80ae10472
afmirandadiaz <amirandadiaz@godaddy.com>
2024-09-04 09:52:03 -0500

    DataStructures

--- configmongo.py
+++ configmongo.py
@@ -1,5 +1,5 @@
 config = {
     'chainconnection':'mongodb+srv://andrewdiazmiranda:3dMEjkl7QsRP42Z2@cluster0.7dlwb0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    'database':'vulnsData',
-    'collection':'softwareByIP'
+    'database':'studentsDC',
+    'collection':'studentsDC'
 }
--- mongoDataload.py
+++ mongoDataload.py
@@ -1,15 +1,23 @@
-import requests, json
+import requests
+from pymongo.mongo_client import MongoClient
+from configmongo import config
 
 url = "https://worldcupjson.net/matches"
 
 response = requests.get(url)
+#Parametros de conexión a mongo Atlas
+cliente = MongoClient(config['chainconnection'])
+datadb = cliente[config['database']]
+tabla = datadb[config['collection']]
 
-#print(json.dumps(response.json()[63],indent=4))
+def createone(estadio,ganador, perdedor, goles):
+    data = {'Estadio':str(estadio),'Ganador':str(ganador),'Perdedor':str(perdedor),'Goles totales':str(goles)}
+    newinput = tabla.insert_one(data)
+    return newinput.inserted_id
 
-print("Estadio: "+response.json()[63]['venue'])
-print("Ganador: "+response.json()[63]['winner'])
-print("Perdedor: "+response.json()[63]['away_team_country'])
-print("Goles: "+str(response.json()[63]['home_team']['goals'] + response.json()[63]['home_team']['penalties']))
+goles = response.json()[63]['home_team']['goals'] + response.json()[63]['home_team']['penalties']
+createone(response.json()[63]['venue'],response.json()[63]['winner'],response.json()[63]['away_team_country'],goles)
+
 
 
 
